530427ca826eee64d525f76edff6ef4ec194cf1c
ketjehdev <ketjeh.developer@gmail.com>
2022-08-07 10:05:06 +0700

    update homepage footer

--- js/splash.js
+++ js/splash.js
@@ -3,5 +3,5 @@ const splash = document.querySelector(".splash");
 document.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     splash.classList.add("display-none");
-  }, 800);
+  }, 900);
 });
