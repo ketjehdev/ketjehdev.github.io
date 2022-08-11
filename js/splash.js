88953f6a9bc1a7eb247d0394db4637f735487912
ketjehdev <ketjeh.developer@gmail.com>
2022-08-11 22:47:05 +0700

    add beberapa pages & update service page

--- js/splash.js
+++ js/splash.js
@@ -3,5 +3,5 @@ const splash = document.querySelector(".splash");
 document.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     splash.classList.add("display-none");
-  }, 900);
+  }, 500);
 });
